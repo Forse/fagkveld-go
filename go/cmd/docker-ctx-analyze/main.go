6fa31d5ec4a9fd5d02ceeca2df14c07d4c5ac13a
Martin Othamar <martin@othamar.net>
2023-05-04 02:01:32 +0200

    docker-ctx-analyze initial solution

--- go/cmd/docker-ctx-analyze/main.go
+++ go/cmd/docker-ctx-analyze/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"flag"
+	"log"
 	"os"
-	"path"
+	"path/filepath"
+
+	"github.com/codeskyblue/dockerignore"
 )
 
 // This CLI should figure out how large the build context is for a given Dockerfile and context dir.
@@ -12,25 +16,106 @@ import (
 // It should print the size of the context in bytes to stdout.
 
 func main() {
-	pwd, err := os.Getwd()
+	errorLogger := log.New(os.Stderr, "ERROR: ", 0)
+	infoLogger := log.New(os.Stdout, "INFO: ", 0)
+
+	config, err := getConfig(errorLogger)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: could not get current working directory: '%s'\n", err)
-		os.Exit(1)
+		errorLogger.Fatalf("could not read configuration: %s", err)
 	}
 
-	// Check if a file named 'Dockerfile' exists in the pwd directory
-	// The filepath for Dockerfile should come from a command line argument
-	filePath := path.Join(pwd, "Dockerfile")
-	_, err = os.Stat(filePath)
+	infoLogger.Printf("analyzing context based on Dockerfile='%s', context directory='%s'", config.dockerfile, config.context)
+
+	_, err = os.Stat(config.context)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: Dockerfile not found: '%s'\n", err)
-		os.Exit(1)
+		errorLogger.Fatalf("could not read context directory: %s", err)
 	}
 
-	// 1. Check that the context path exists
-	// 2. Check that the context path is a directory
-	// 3. Find the .dockerignore file in the context path
-	// 4. Traverse the context path and sum the size of all files that are not ignored by .dockerignore
+	_, err = os.Stat(config.dockerfile)
+	if err != nil {
+		errorLogger.Fatalf("could not read Dockerfile: %s", err)
+	}
 
-	os.Exit(0)
+	dockerIgnorePath := filepath.Join(config.context, ".dockerignore")
+
+	_, err = os.Stat(dockerIgnorePath)
+	if err != nil {
+		errorLogger.Fatalf("could not read .dockerignore: %s", err)
+	}
+
+	ignorePatterns, err := dockerignore.ReadIgnoreFile(dockerIgnorePath)
+	if err != nil {
+		errorLogger.Fatalf("could not read .dockerignore: %s", err)
+	}
+
+	infoLogger.Printf("ignore patterns: %v", ignorePatterns)
+
+	var ignoredSize int64 = 0
+	var includedSize int64 = 0
+	var files = 0
+
+	err = filepath.Walk(config.context, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			errorLogger.Fatalf("could not read file: %s", err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		relativePath, err := filepath.Rel(config.context, path)
+		if err != nil {
+			return err
+		}
+
+		isMatch, err := dockerignore.Matches(relativePath, ignorePatterns)
+		if err != nil {
+			return err
+		}
+
+		infoLogger.Printf("file: %s", relativePath)
+
+		files += 1
+		if isMatch {
+			ignoredSize += info.Size()
+		} else {
+			includedSize += info.Size()
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		errorLogger.Fatalf("could not read context directory: %s", err)
+	}
+
+	infoLogger.Printf("files: %d", files)
+	infoLogger.Printf("context size: %d bytes", includedSize)
+	infoLogger.Printf("ignored size: %d bytes", ignoredSize)
+}
+
+type config struct {
+	dockerfile string
+	context    string
+}
+
+func getConfig(errorLogger *log.Logger) (*config, error) {
+	dockerfileFlag := flag.String("f", "./Dockerfile", "Dockerfile filepath - defaults to './Dockerfile'")
+
+	flag.Parse()
+
+	dockerContextPathFlag := flag.Arg(0)
+
+	if dockerfileFlag == nil || *dockerfileFlag == "" {
+		return nil, errors.New("dockerfile filepath not specified")
+	}
+
+	if dockerContextPathFlag == "" {
+		return nil, errors.New("docker context directory not specified")
+	}
+
+	return &config{
+		dockerfile: *dockerfileFlag,
+		context:    dockerContextPathFlag,
+	}, nil
 }
--- go/go.mod
+++ go/go.mod
@@ -2,17 +2,18 @@ module fagkveld
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20230321023759-10a507213a29
+require (
+	github.com/codeskyblue/dockerignore v0.0.0-20151214070507-de82dee623d9
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/stretchr/testify v1.8.2
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
-	go.opentelemetry.io/otel v1.15.1 // indirect
-	go.opentelemetry.io/otel/trace v1.15.1 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
